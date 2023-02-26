2ae2824682be37869aa3df270afa7c1a90e0151f
sokkanen <joel.sokkanen@helsinki.fi>
2023-02-27 00:44:07 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        int maxN = 1000;
+        Random rnd = new Random();
+        int[] randomNumbers = rnd.ints(1000, 1, 1001).toArray();
+        long[] timeSamples = new long[maxN];
+        for (int i = 0; i < 1000; i++) {
+            long startTime = System.currentTimeMillis();
+            int sumCount = rnd.nextInt(maxN + 1) + 1;
+            int finalI = i;
+            Thread t = new Thread() {
+                public void run() {
+                    threadFunction(randomNumbers, sumCount);
+                }
+            };
+            t.start();
+            long endTime = System.currentTimeMillis();
+            timeSamples[finalI] = endTime - startTime;
+        }
+        printStatistics(timeSamples, maxN);
+    }
+
+    public static void printStatistics(long[] timeSamples, int maxN) {
+        long min = Arrays.stream(timeSamples).min().getAsLong();
+        long max = Arrays.stream(timeSamples).max().getAsLong();
+        double avg = Arrays.stream(timeSamples).average().getAsDouble();
+        long mean = Arrays.stream(timeSamples).sorted().toArray()[maxN / 2];
+        System.out.println(mean);
+        double stdDev = Math.sqrt(
+                Arrays.stream(timeSamples)
+                        .mapToDouble(num -> Math.abs(mean - (double)num))
+                        .map(val -> Math.sqrt(val))
+                        .sum() / timeSamples.length
+        );
+        System.out.printf("Minimum value: %d, Maximum value: %d, Average: %f, Standard deviation: %f", min, max, avg, stdDev);
+    }
+
+    public static void threadFunction (int[] numbers, int sumCount) {
+        Arrays.stream(numbers).limit(sumCount).sum();
+    }
+}
