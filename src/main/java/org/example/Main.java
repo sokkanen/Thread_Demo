b797e2e23a9695e809fc52049c8a18c90cec7fd1
sokkanen <joel.sokkanen@helsinki.fi>
2023-02-27 18:20:30 +0200

    Option to perform operations without starting a new thread.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws Exception{
+        boolean runInThread = false;
         int maxN = 1000;
         Random rnd = new Random();
         int[] randomNumbers = rnd.ints(1000, 1, 1001).toArray();
@@ -12,15 +13,22 @@ public class Main {
         for (int i = 0; i < 1000; i++) {
             long startTime = System.currentTimeMillis();
             int sumCount = rnd.nextInt(maxN + 1) + 1;
-            int finalI = i;
-            Thread t = new Thread() {
-                public void run() {
-                    threadFunction(randomNumbers, sumCount);
-                }
-            };
-            t.start();
-            long endTime = System.currentTimeMillis();
-            timeSamples[finalI] = endTime - startTime;
+            if (runInThread) {
+                int finalI = i;
+                Thread t = new Thread() {
+                    public void run() {
+                        arraySum(randomNumbers, sumCount);
+                    }
+                };
+                t.start();
+                long endTime = System.currentTimeMillis();
+                timeSamples[finalI] = endTime - startTime;
+            } else {
+                arraySum(randomNumbers, sumCount);
+                long endTime = System.currentTimeMillis();
+                timeSamples[i] = endTime - startTime;
+            }
+
         }
         printStatistics(timeSamples, maxN);
     }
@@ -40,7 +48,7 @@ public class Main {
         System.out.printf("Minimum value: %d, Maximum value: %d, Average: %f, Standard deviation: %f", min, max, avg, stdDev);
     }
 
-    public static void threadFunction (int[] numbers, int sumCount) {
+    public static void arraySum(int[] numbers, int sumCount) {
         Arrays.stream(numbers).limit(sumCount).sum();
     }
 }
